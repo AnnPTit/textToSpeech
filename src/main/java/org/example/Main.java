07031bf0fec5d2250fa217093ce0e800196f0363
an <phamthanhanzwz@gmail.com>
2025-02-20 22:46:44 +0700

    hehe

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,17 @@
 package org.example;
 
 import org.example.service.MainService;
-
-import java.io.IOException;
-
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+@SpringBootApplication
+@EnableConfigurationProperties({ LiquibaseProperties.class})
 public class Main {
     private static MainService mainService = new MainService();
 
     public static void main(String[] args) throws Exception {
+        SpringApplication.run(MainService.class, args);
         mainService.showMenu();
     }
 }
