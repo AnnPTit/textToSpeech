6a2a8366ec85e4c13618830479e0aaf5b6d8336a
AnHandSome <anpt@techasians.com>
2025-03-26 17:25:21 +0700

    Them kafka

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,8 @@
 package org.example;
 
 import org.example.service.MainService;
+import org.example.service.kafka.KafkaAdminClientUtil;
+import org.example.service.kafka.KafkaConsumerService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
@@ -10,10 +12,15 @@ import org.springframework.context.ApplicationContext;
 @SpringBootApplication
 @EnableConfigurationProperties({LiquibaseProperties.class})
 public class Main {
+
     public static void main(String[] args) throws Exception {
         ApplicationContext context = SpringApplication.run(Main.class, args);
         // Lấy MainService từ Spring Context
-        MainService mainService = context.getBean(MainService.class);
-        mainService.showMenu();
+//        MainService mainService = context.getBean(MainService.class);
+        //        mainService.showMenu();
+        KafkaAdminClientUtil kafkaAdminClientUtil = context.getBean(KafkaAdminClientUtil.class);
+        System.out.println("hehehehe: " + kafkaAdminClientUtil.getAllTopics());
+        System.out.println("hohojo: " + kafkaAdminClientUtil.getAllBrokers());
+        System.out.println("kekeke: " + kafkaAdminClientUtil.getClusterId());
     }
 }
